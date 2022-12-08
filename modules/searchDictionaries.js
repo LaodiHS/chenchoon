3ab84ef243b213cedbdaaea9631852abad4a7688
wujimu <hasanseirafi69@gmail.com>
2022-12-08 06:51:53 -0800

    include sub dir files

--- modules/searchDictionaries.js
+++ modules/searchDictionaries.js
@@ -1,32 +1,27 @@
+const { dir, readFile, read_dir } = require("./personalized_file_system.js");
 
-const{ dir, readFile, read_dir} = require('./personalized_file_system.js')
-
-
-const cache = {}
+const cache = {};
 async function search(req, response) {
   try {
     let word = req.params.word.split("");
     let limit = req.params.limit;
-    const api = req.params.api
+    const api = req.params.api;
     if (cache[req.params.word + req.params.api + req.params.limit])
-      return response.status(200).json(cache[req.params.word + req.params.api + req.params.limit]);
-     
-      let c = await read_dir(dir);
-      console.log('read_dir===>',dir+'dic','------>', c);
-   const dir_i = dir + `dic/${api}/${word.shift()}.json`;
-console.log('dir->', dir_i)
-   let root = await JSON.parse(await readFile(dir_i, "utf8"));
-   console.log('root', root);
+      return response
+        .status(200)
+        .json(cache[req.params.word + req.params.api + req.params.limit]);
+    let c = await read_dir(dir);
+    const dir_i = dir + `dic/${api}/${word.shift()}.json`;
+    let root = await JSON.parse(await readFile(dir_i, "utf8"));
+    console.log("root", root);
     let temp_root = root;
     let path = word.length;
     let results = [];
     for (let letter of word) {
       if (!temp_root[letter]) {
         return response.sendStatus(404);
-
       }
-      temp_root = temp_root[letter]
-
+      temp_root = temp_root[letter];
     }
     const queue = [temp_root];
     while (queue.length) {
@@ -39,15 +34,14 @@ console.log('dir->', dir_i)
       queue.length = results.length >= limit ? 0 : queue.length;
     }
 
-    results.length ? response.status(200).json(results) : response.sendStatus(404);
+    results.length
+      ? response.status(200).json(results)
+      : response.sendStatus(404);
     cache[req.params.word + req.params.api + req.params.limit] = results;
   } catch (error) {
-    console.log('error', error);
+    console.log("error", error);
     response.sendStatus(404);
   }
 }
 
-
-
-
-module.exports = { search: search}
+module.exports = { search: search };
